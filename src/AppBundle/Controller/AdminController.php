1d046afd320ae31b4911b2942cd196445fd09e98
Fabian Schmick <schmick.fabian@web.de>
2017-10-22 14:02:25 +0200

    Secure admin path with login.

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Admin controller.
+ *
+ * @Route("admin")
+ */
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="admin_index")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('admin/index.html.twig', [
+
+        ]);
+    }
+}
