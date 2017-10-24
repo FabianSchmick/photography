f353dd695efd9e32c52e80eb561829d315f73808
Fabian Schmick <schmick.fabian@web.de>
2017-10-24 22:39:49 +0200

    Added npm packages and gulp configuration.

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+/* Gulp */
+var gulp  = require('gulp'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    rev = require('gulp-rev'),
+    del = require('del');
+    es = require('event-stream'),
+    rs = require('run-sequence');
+
+/* Third Party */
+var copy = {
+    fonts: {
+        src: [
+            './node_modules/font-awesome/fonts/*'
+        ],
+        dest: './web/bundles/app/min/fonts/'
+    }
+};
+
+/* Tasks */
+gulp.task('fonts', function () {
+    gulp.src(copy.fonts.src)
+        .pipe(gulp.dest(copy.fonts.dest));
+});
+
+gulp.task('styles', function() {
+    return es.concat(
+        gulp.src([
+            './node_modules/font-awesome/less/font-awesome.less',
+            './node_modules/animate.css/animate.css',
+            './node_modules/fancybox/css/jquery.fancybox.css',
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+        ])
+        .pipe(plumber(function (error) {
+            console.log(error.toString());
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init()),
+        gulp.src([
+            './web/bundles/app/less/default.less'
+        ])
+        .pipe(plumber(function (error) {
+            console.log(error.toString());
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(less())
+    )
+    .pipe(concat('min/css/main.css' ))
+    .pipe(uglifycss())
+    .pipe(rev())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('web/bundles/app'))
+    .pipe(rev.manifest({merge: true}))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/enquire.js/dist/enquire.js',
+        './node_modules/fancybox/js/jquery.fancybox.js',
+        './node_modules/jquery-match-height/dist/jquery.matchHeight.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './web/bundles/app/js/default.js'
+    ])
+    .pipe(plumber(function (error) {
+        console.log(error.toString());
+        this.emit('end');
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(concat('min/js/main.js'))
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('web/bundles/app'))
+    .pipe(rev.manifest({merge: true}))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('clean', function () {
+    return del.sync(['rev-manifest.json', 'web/bundles/app/min/css/*', 'web/bundles/app/min/js/*']);
+});
+
+gulp.task('watch', function() {
+    gulp.watch( './web/bundles/app/less/**', function () {
+        rs(
+            'clean',
+            'styles',
+            'scripts'
+        )
+    });
+    gulp.watch( './web/bundles/app/js/**', function () {
+        rs(
+            'clean',
+            'styles',
+            'scripts'
+        )
+    });
+});
+
+gulp.task('default',
+    function () {
+        rs(
+            'clean',
+            'fonts',
+            'styles',
+            'scripts'
+        )
+    }
+);
