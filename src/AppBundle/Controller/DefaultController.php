46ef788cc6e5bf93b3f36b16ccdcfb9549c37471
Fabian Schmick <schmick.fabian@web.de>
2017-10-28 00:24:39 +0200

    First html and styles for frontend page.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('frontend/index.html.twig', [
         ]);
     }
 }
--- src/AppBundle/Resources/public/js/default.js
+++ src/AppBundle/Resources/public/js/default.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+    parallax();
 });
+
+function parallax() {
+    var parallaxElements = $('.parallax'),
+        parallaxQuantity = parallaxElements.length;
+
+    $(window).on('scroll', function () {
+        window.requestAnimationFrame(function () {
+
+            for (var i = 0; i < parallaxQuantity; i++) {
+                var currentElement = parallaxElements.eq(i);
+                var scrolled = $(window).scrollTop();
+
+                currentElement.css({
+                    'transform': 'translate3d(0,' + scrolled * -0.3 + 'px, 0)'
+                });
+            }
+        });
+    });
+}
