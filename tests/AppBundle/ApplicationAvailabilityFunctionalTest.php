f55669a26bf2c0dcb8b968aec41193ff8ef5a468
Fabian Schmick <schmick.fabian@web.de>
2019-01-12 18:19:29 +0100

    Added more urls to test.

--- tests/AppBundle/ApplicationAvailabilityFunctionalTest.php
+++ tests/AppBundle/ApplicationAvailabilityFunctionalTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Define a functional test that at least checks if your application pages are successfully loading.
@@ -23,6 +24,18 @@ class ApplicationAvailabilityFunctionalTest extends WebTestCase
     }
 
     /**
+     * @dataProvider getSecureUrls
+     */
+    public function testSecureUrls($url)
+    {
+        $client = static::createClient();
+
+        $client->request('GET', $url);
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+    }
+
+    /**
      * Some pages
      */
     public function urlProvider()
@@ -34,5 +47,15 @@ class ApplicationAvailabilityFunctionalTest extends WebTestCase
         yield ['/de/tour/page/1'];
         yield ['/en/tour/page/1'];
         yield ['/sitemap.xml'];
+    }
+
+    /**
+     * Secure pages
+     */
+    public function getSecureUrls()
+    {
+        yield ['/admin/'];
+        yield ['/admin/entry/new'];
+        yield ['/admin/language/en'];
     }
 }
