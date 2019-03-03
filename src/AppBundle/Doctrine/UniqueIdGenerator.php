46f57b87a3a74ca8e56b1fa21e2c60ab847bcae3
Fabian Schmick <schmick.fabian@web.de>
2019-03-03 14:32:29 +0100

    Look for ids in uow, too.

--- src/AppBundle/Doctrine/UniqueIdGenerator.php
+++ src/AppBundle/Doctrine/UniqueIdGenerator.php
@@ -15,6 +15,12 @@ class UniqueIdGenerator extends AbstractIdGenerator
         do {
             $id = random_int(10000000, 99999999);
             $found = $em->getRepository(get_class($entity))->findOneBy(['id' => $id]);
+
+            if (!$found) {
+                $persisted = $em->getUnitOfWork()->getScheduledEntityInsertions();
+                $ids = array_map(function($o) { return $o->getId(); }, $persisted);
+                $found = array_search($id, $ids);
+            }
         } while ($found);
 
         return $id;
