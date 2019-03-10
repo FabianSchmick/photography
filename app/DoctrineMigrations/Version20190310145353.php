819a2dd1eb9c7e0c232c9876e1e8dfea74a96a5a
Fabian Schmick <schmick.fabian@web.de>
2019-03-10 17:26:03 +0100

    Removed post-method.

--- app/DoctrineMigrations/Version20190310145353.php
+++ app/DoctrineMigrations/Version20190310145353.php
@@ -4,25 +4,12 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190310145353 extends AbstractMigration implements ContainerAwareInterface
+final class Version20190310145353 extends AbstractMigration
 {
-    /**
-     * @var EntityManagerInterface $em
-     */
-    private $em;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->em = $container->get('doctrine.orm.entity_manager');
-    }
-
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
@@ -31,29 +18,6 @@ final class Version20190310145353 extends AbstractMigration implements Container
         $this->addSql('ALTER TABLE entry ADD created DATETIME NOT NULL DEFAULT NOW(), ADD updated DATETIME NOT NULL DEFAULT NOW()');
         $this->addSql('ALTER TABLE tag ADD created DATETIME NOT NULL DEFAULT NOW(), ADD updated DATETIME NOT NULL DEFAULT NOW()');
         $this->addSql('ALTER TABLE tour ADD created DATETIME NOT NULL DEFAULT NOW()');
-    }
-
-    public function postUp(Schema $schema)
-    {
-        $entries = $this->em->getRepository('AppBundle:Entry')->findAll();
-        foreach ($entries as $entry) {
-            $entry->setCreated($entry->getTimestamp());
-            $this->em->persist($entry);
-        }
-
-        $tags = $this->em->getRepository('AppBundle:Tag')->findAll();
-        foreach ($tags as $tag) {
-            $tag->setCreated(new \DateTime());
-            $this->em->persist($tag);
-        }
-
-        $tours = $this->em->getRepository('AppBundle:Tour')->findAll();
-        foreach ($tours as $tour) {
-            $tour->setCreated($tour->getUpdated());
-            $this->em->persist($tour);
-        }
-
-        $this->em->flush();
     }
 
     public function down(Schema $schema) : void
