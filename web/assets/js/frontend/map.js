7971f0ad52978b7b9b454483d9403d8c54269162
Fabian Schmick <schmick.fabian@web.de>
2019-11-30 23:56:22 +0100

    Fixed map markers for webpack.

--- web/assets/js/frontend/map.js
+++ web/assets/js/frontend/map.js
@@ -1,5 +1,8 @@
 import * as L from 'leaflet/src/leaflet';
 import * as omnivore from '@mapbox/leaflet-omnivore/index';
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 /**
  * Create a new map with gpx track
@@ -17,6 +20,16 @@ export function map() {
 
     map.on('focus', () => { map.scrollWheelZoom.enable(); });
     map.on('blur', () => { map.scrollWheelZoom.disable(); });
+
+    // stupid hack so that leaflet's images work after going through webpack
+    // https://github.com/PaulLeCam/react-leaflet/issues/255#issuecomment-388492108
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: marker2x,
+        iconUrl: marker,
+        shadowUrl: markerShadow
+    });
 
     L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
         maxZoom: 18,
