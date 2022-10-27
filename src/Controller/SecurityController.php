1600cb7801cce00f72962c6f4ab94c05d64e7be7
FabianSchmick <FabianSchmick@users.noreply.github.com>
2022-10-27 06:52:57 +0000

    Fix CS with PHP-CS-Fixer

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use DateTime;
 
 class SecurityController extends AbstractController
 {
