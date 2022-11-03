bbc4dbf14dbc037e95b1e36ce661e7a8cb00b447
Fabian <schmick.fabian@web.de>
2022-11-03 15:09:36 +0100

    Implement enum for the tour duration-formula (#45)

---
+++ src/Config/TourDurationFormula.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Config;
+
+enum TourDurationFormula
+{
+    case HIKING;
+    case MTB;
+    case VIA_FERRATA;
+
+    public function constants(): array
+    {
+        return match ($this) {
+            TourDurationFormula::HIKING => [
+                /* Values according to: DIN 33466 */
+                'UP_METERS_PER_HOUR' => 300,
+                'DOWN_METERS_PER_HOUR' => 500,
+                'HORIZONTAL_METERS_PER_HOUR' => 4,
+            ],
+            TourDurationFormula::MTB => [
+                'UP_METERS_PER_HOUR' => 500,
+                'HORIZONTAL_METERS_PER_HOUR' => 12,
+            ],
+            TourDurationFormula::VIA_FERRATA => [
+                'UP_METERS_PER_HOUR' => 200,
+                'DOWN_METERS_PER_HOUR' => 400,
+            ],
+        };
+    }
+}
